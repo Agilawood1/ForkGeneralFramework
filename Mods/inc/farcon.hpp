7e20bb9e6ac179f013594cb206086292b9581e5f
Kaneki Ken <1979716201@qq.com>
2026-01-27 15:50:19 +0800

    遥控器合并2

--- Mods/inc/farcon.hpp
+++ Mods/inc/farcon.hpp
@@ -5,9 +5,18 @@
 
 #ifndef _FARCON_HPP_
 #define _FARCON_HPP_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "stm32f4xx_hal.h"
 
 #include "bsp_uart.h"
+
+#ifdef __cplusplus
+}
+#endif
 
 #define pos_data_length 10
 class Farcon
